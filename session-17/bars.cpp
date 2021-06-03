cccde3efc535451864e6d6ad3bb2f1ffe143e3e3
muhammad-magdi <muhammad.magdi1024@gmail.com>
2021-06-03 23:37:56 +0200

    add backTracking session codes

---
+++ session-17/bars.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 10;
+
+int n, target;
+int A[N];
+
+int sum;
+vector<int> path;
+void solve(int i) {
+    if (i == n) {
+        if (sum == target) {
+            for (int i = 0; i < path.size(); i++) {
+                printf("%d%c", path[i], " \n"[i+1==path.size()]);
+            }
+        }
+        return;
+    }
+    // Option 1 => Pick
+    if (sum + A[i] <= target) {     // Valid
+        sum += A[i];    // Do
+        path.push_back(A[i]);
+        solve(i+1);     // Recurse
+        sum -= A[i];    // Undo
+        path.pop_back();
+    }
+    // Option 2 => Leave
+    // Do
+    solve(i+1); // Recurse
+    // Undo
+}
+
+int main() {
+    scanf("%d %d", &n, &target);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", A+i);
+    }
+    solve(0);
+    return 0;
+}
+/*
+925
+10
+45 15 120 500 235 58 6 12 175 70
+*/
---
+++ session-17/rat-in-maze.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 10;
+
+int n, m;
+char grid[N][N];
+string path;
+
+// Foreach valid option
+//  Do
+//  Recurse
+//  Undo
+void solve(int r, int c) {
+    if (r == n-1 && c == m-1) { // Base Case => Found Cheese
+        printf("%s\n", path.c_str());
+        return;
+    }
+    // Option 1 = Right
+    if (c+1 < m && grid[r][c+1] != '#') {   // Valid
+        path.push_back('R');    // Do
+        solve(r, c+1);          // Recurse
+        path.pop_back();        // Undo
+    }
+    // Option 2 = Down
+    if (r+1 < n && grid[r+1][c] != '#') {   // Valid
+        path.push_back('D');    // Do
+        solve(r+1, c);          // Recurse
+        path.pop_back();        // Undo
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        scanf("%s", grid[i]);
+    }
+    solve(0, 0);
+    return 0;
+}
