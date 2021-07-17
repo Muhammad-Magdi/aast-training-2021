3f21d1967cec209f6b1556bb0216527903793572
muhammad-magdi <muhammad.magdi1024@gmail.com>
2021-07-17 03:23:52 +0200

    add session-21 codes

---
+++ session-21/dueling-philosophers.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int NO_SOLUTIONS = 0, ONE_SOLUTION = 1, MORE_SOLUTIONS = 2;
+
+const int N = 1e3 + 5, M = 5e4 + 5, OO = 0x3f3f3f3f;
+
+vector<int> adj[N];
+int n, m, d, u;
+int IN[N];
+// int vis[N], testCase;
+
+int BFS() {
+    int cnt = 0;
+    bool haveOptions = false;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        if (IN[i] == 0) {
+            q.push(i);
+            // vis[i] = testCase;
+        }
+    }
+
+    while (!q.empty()) {
+        if (q.size() > 1) haveOptions = true;
+        int u = q.front();
+        cnt++;
+        q.pop();
+        for (int v: adj[u]) {
+            IN[v]--;
+            if (IN[v] == 0 /*&& vis[v] != testCase*/) {
+                q.push(v);
+                // vis[v] = testCase;
+            }
+        }
+    }
+    if (cnt != n)   return NO_SOLUTIONS;
+    if (haveOptions)    return MORE_SOLUTIONS;
+    return ONE_SOLUTION;
+}
+
+void reset() {
+    for (int i = 1; i <= n; i++) adj[i].clear();
+    // memset(vis, false, sizeof vis);
+    // testCase++;
+    memset(IN, 0, sizeof IN);
+}
+
+int main() {
+    while(scanf("%d %d", &n, &m), n || m) {
+        reset();
+        for (int i = 0; i < m; i++) {
+            scanf("%d %d", &d, &u);
+            adj[d].push_back(u);
+            IN[u]++;
+        }
+        printf("%d\n", BFS());
+    }
+    return 0;
+}
+
+/*
+Sample input:
+5 4
+1 5
+5 2
+3 2
+4 3
+5 4
+3 1
+4 2
+1 5
+5 4
+2 2
+1 2
+2 1
+0 0
+*/
---
+++ session-21/tree-diameter.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e4 + 5, M = 2e5 + 5;
+
+int n, u, v;
+
+vector<int> adj[N];
+bool vis[N];
+
+int farthestNode;
+int longestPathLength;
+
+void DFS(int u, int depth) {       // O(n + m)
+    vis[u] = true;
+    for (int v : adj[u]) {
+        if (!vis[v]) {
+            DFS(v, depth + 1);
+        }
+    }
+    if (depth > longestPathLength) {
+        farthestNode = u;
+        longestPathLength = depth;
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n-1; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    DFS(1, 0);
+    
+    memset(vis, false, sizeof vis);
+    longestPathLength = 0;
+    DFS(farthestNode, 0);
+    
+    printf("%d\n", longestPathLength);
+    return 0;
+}
+
+/*
+Samples:
+7 6
+0 1
+1 2
+2 5
+2 3
+2 6
+3 4
+*/
